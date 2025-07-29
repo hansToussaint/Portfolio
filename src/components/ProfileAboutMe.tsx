0914e0343dab1373f4dee5c6018c6d18f4312007
Hans Toussaint <hanst605@gmail.com>
2025-07-29 10:57:55 +0300

    Resume update

--- src/components/ProfileAboutMe.tsx
+++ src/components/ProfileAboutMe.tsx
@@ -16,45 +16,70 @@ const ProfileAboutMe: React.FC = () => {
         />
       </Box>
 
-      <Box sx={{
-         mt: 3, 
-         maxWidth: "800px", 
-         mx: "auto", 
-         textAlign: 
-         "left" 
-         }}>
+      <Box
+        sx={{
+          mt: 3,
+          maxWidth: "800px",
+          mx: "auto",
+          textAlign: "left",
+        }}
+      >
         {/* Section "About Me" */}
         <Typography variant="h1" gutterBottom mb={2}>
           About Me
         </Typography>
         <Typography variant="body1" sx={{ textIndent: "2em" }} mb={2}>
-          I am a dedicated developer with a rich and diverse background that spans design, programming, and project management. My journey in technology is fueled by passion and a constant desire to innovate.
+          I am a dedicated developer with a rich and diverse background that
+          spans design, programming, and project management. My journey in
+          technology is fueled by passion and a constant desire to innovate.
         </Typography>
         <Typography variant="body1" mb={2}>
-          Over the years, I have honed my skills through a variety of projects, from working with dynamic startups to collaborating with established companies. This experience has endowed me with a deep understanding of both creative design and technical implementation.
+          Over the years, I have honed my skills through a variety of projects,
+          from working with dynamic startups to collaborating with established
+          companies. This experience has endowed me with a deep understanding of
+          both creative design and technical implementation.
         </Typography>
         <Typography variant="body1" mb={2}>
-          I thrive on challenges and am continually exploring new technologies and methodologies to elevate my work. Whether it’s crafting a user-friendly interface or developing a complex application, I combine precision with creativity to deliver solutions that are as functional as they are beautiful.
+          I thrive on challenges and am continually exploring new technologies
+          and methodologies to elevate my work. Whether it’s crafting a
+          user-friendly interface or developing a complex application, I combine
+          precision with creativity to deliver solutions that are as functional
+          as they are beautiful.
         </Typography>
         <Typography variant="body1" mb={2}>
-          I believe that technology has the power to transform ideas into reality, and I am dedicated to making that transformation seamless and impactful for every client.
+          I believe that technology has the power to transform ideas into
+          reality, and I am dedicated to making that transformation seamless and
+          impactful for every client.
         </Typography>
 
         {/* Section "My Education" */}
         <Typography variant="h1" gutterBottom mb={2}>
           My Education
         </Typography>
-        <Box sx={{ borderLeft: `3px solid ${theme.palette.common.redColor}`, pl: 3, mb: 4, ml: 5.6 }}>
-         <Box sx={{ mb: 2 }}>
+        <Box
+          sx={{
+            borderLeft: `3px solid ${theme.palette.common.redColor}`,
+            pl: 3,
+            mb: 4,
+            ml: 5.6,
+          }}
+        >
+          <Box sx={{ mb: 2 }}>
             <Typography variant="subtitle2">Jan 2022 - Sept 2022</Typography>
             <Typography>Web Development</Typography>
-            <Typography color="inherit" fontWeight={400}>Online Bootcamp</Typography>
+            <Typography color="inherit" fontWeight={400}>
+              Online Bootcamp
+            </Typography>
           </Box>
 
           <Box sx={{ mb: 2 }}>
             <Typography variant="subtitle2">Sept 2020 - June 2024</Typography>
-            <Typography>Bachelor in Chemistry, <i>Mendeleiev University</i></Typography>
-            <Typography color="inherit" fontWeight={400}>Moscow, Russia</Typography>
+            <Typography>
+              Bachelor, <i>Mendeleiev University</i>
+            </Typography>
+            <Typography color="inherit" fontWeight={400}>
+              Moscow, Russia
+            </Typography>
           </Box>
 
           {/* <Box sx={{ mb: 2 }}>
@@ -68,7 +93,6 @@ const ProfileAboutMe: React.FC = () => {
             <Typography>High School, <i>College Saint Martin de Tours</i></Typography>
             <Typography color="inherit" fontWeight={400}>Port-au-Prince, Haiti</Typography>
           </Box> */}
-
         </Box>
 
         {/* Section "Why Choose Me" */}
@@ -76,13 +100,24 @@ const ProfileAboutMe: React.FC = () => {
           Why Choose Me?
         </Typography>
         <Typography variant="body1" sx={{ textIndent: "2em" }} mb={2}>
-          I bring a unique blend of creativity, technical expertise, and an uncompromising work ethic to every project I undertake. My approach combines innovative thinking with solid technical foundations, ensuring that every solution is not only visually engaging but also robust and efficient.
+          I bring a unique blend of creativity, technical expertise, and an
+          uncompromising work ethic to every project I undertake. My approach
+          combines innovative thinking with solid technical foundations,
+          ensuring that every solution is not only visually engaging but also
+          robust and efficient.
         </Typography>
         <Typography variant="body1" mb={2}>
-          I have a proven track record of delivering projects that exceed expectations by paying close attention to every detail—from initial concept to final execution. I continuously update my skill set with the latest industry trends and best practices, allowing me to offer cutting-edge solutions that are tailored to your specific needs.
+          I have a proven track record of delivering projects that exceed
+          expectations by paying close attention to every detail—from initial
+          concept to final execution. I continuously update my skill set with
+          the latest industry trends and best practices, allowing me to offer
+          cutting-edge solutions that are tailored to your specific needs.
         </Typography>
         <Typography variant="body1" mb={2}>
-          My commitment to clear communication and collaborative problem-solving means that I work closely with clients throughout the process, ensuring that each project reflects their vision while also pushing the boundaries of what’s possible.
+          My commitment to clear communication and collaborative problem-solving
+          means that I work closely with clients throughout the process,
+          ensuring that each project reflects their vision while also pushing
+          the boundaries of what’s possible.
         </Typography>
       </Box>
     </Box>
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -6,31 +6,31 @@ import ProfileAboutMe from "../components/ProfileAboutMe";
 import AppMenu from "../components/AppMenu";
 
 const AboutMe: React.FC = () => {
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
 
-    return (
-        <Box sx={{bgcolor: theme.palette.common.white,}}>
-          <AppMenu activeSection="about" />
-          <Box
-            sx={{
-              padding: isSmallScreen ? "1.6rem" : "5rem 0.5rem",
-              maxWidth: "1150px",
-              width: "100%",
-              margin: "0 auto",
-            }}
-          >
-            <Grid2 container spacing={isSmallScreen ? 3 : 12}>
-              <Grid2 size={{ xs: 12, md: 4 }} sx={{ order: { xs: 2, md: 1 } }}>
-                <MotivationTextCard />
-                <ContactCard />
-              </Grid2>
-              <Grid2 size={{ xs: 12, md: 8 }} sx={{ order: { xs: 1, md: 2 } }}>
-                <ProfileAboutMe />
-              </Grid2>
-            </Grid2>
-          </Box>
-        </Box>
-    );
+  return (
+    <Box sx={{ bgcolor: theme.palette.common.white }}>
+      <AppMenu activeSection="about" />
+      <Box
+        sx={{
+          padding: isSmallScreen ? "1.6rem" : "5rem 0.5rem",
+          maxWidth: "1150px",
+          width: "100%",
+          margin: "0 auto",
+        }}
+      >
+        <Grid2 container spacing={isSmallScreen ? 3 : 12}>
+          <Grid2 size={{ xs: 12, md: 4 }} sx={{ order: { xs: 2, md: 1 } }}>
+            <MotivationTextCard />
+            <ContactCard />
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 8 }} sx={{ order: { xs: 1, md: 2 } }}>
+            <ProfileAboutMe />
+          </Grid2>
+        </Grid2>
+      </Box>
+    </Box>
+  );
 };
 
 export default AboutMe;
